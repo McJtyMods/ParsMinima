125ea9ec928bb4930bbd3ebd984f04a3ea8ff971
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2024-02-16 18:54:18 +0100

    Lot of work towards pre-porting to 1.20.4 NeoForge

--- src/main/java/mcjty/parsminima/common/Registration.java
+++ src/main/java/mcjty/parsminima/common/Registration.java
@@ -13,8 +13,8 @@ import static mcjty.parsminima.ParsMinima.MODID;
 
 public class Registration {
 
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredBlocks BLOCKS = DeferredBlocks.create(MODID);
+    public static final DeferredItems ITEMS = DeferredItems.create(MODID);
     public static final DeferredRegister<BlockEntityType<?>> TILES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MODID);
 
     public static void init() {
@@ -24,11 +24,11 @@ public class Registration {
         TILES.register(bus);
     }
 
-    public static final RegistryObject<MultipartBlock> MULTIPART_BLOCK = BLOCKS.register("multipart", MultipartBlock::new);
-    public static final RegistryObject<Item> MULTIPART_ITEM = ITEMS.register("multipart", () -> new MultipartBlockItem(MULTIPART_BLOCK.get(), new Item.Properties()));
-    public static final RegistryObject<BlockEntityType<MultipartTE>> MULTIPART_TILE = TILES.register("multipart", () -> BlockEntityType.Builder.of(MultipartTE::new, MULTIPART_BLOCK.get()).build(null));
+    public static final Supplier<MultipartBlock> MULTIPART_BLOCK = BLOCKS.register("multipart", MultipartBlock::new);
+    public static final Supplier<Item> MULTIPART_ITEM = ITEMS.register("multipart", () -> new MultipartBlockItem(MULTIPART_BLOCK.get(), new Item.Properties()));
+    public static final Supplier<BlockEntityType<MultipartTE>> MULTIPART_TILE = TILES.register("multipart", () -> BlockEntityType.Builder.of(MultipartTE::new, MULTIPART_BLOCK.get()).build(null));
 
-    public static final RegistryObject<MultipartBlock> MINI_BLOCK = BLOCKS.register("mini", MultipartBlock::new);
-    public static final RegistryObject<Item> MINI_ITEM = ITEMS.register("mini", () -> new MultipartBlockItem(MINI_BLOCK.get(), new Item.Properties()));
-    public static final RegistryObject<BlockEntityType<MultipartTE>> MINI_TILE = TILES.register("mini", () -> BlockEntityType.Builder.of(MultipartTE::new, MINI_BLOCK.get()).build(null));
+    public static final Supplier<MultipartBlock> MINI_BLOCK = BLOCKS.register("mini", MultipartBlock::new);
+    public static final Supplier<Item> MINI_ITEM = ITEMS.register("mini", () -> new MultipartBlockItem(MINI_BLOCK.get(), new Item.Properties()));
+    public static final Supplier<BlockEntityType<MultipartTE>> MINI_TILE = TILES.register("mini", () -> BlockEntityType.Builder.of(MultipartTE::new, MINI_BLOCK.get()).build(null));
 }
